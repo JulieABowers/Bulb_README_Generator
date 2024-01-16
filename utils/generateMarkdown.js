e1f8f5f7bd3a0f82500bb8b2b65bd8d3a54dbbd3
JulieABowers <Haldirolorien@comcast.net>
2024-01-15 21:16:36 -0600

    Add pseudo code

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,10 +1,28 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  /* Badge links
+    
+    Apache 2.0: https://img.shields.io/badge/License-Apache%202.0-blue.svg
+    GNU GPL 3.0: https://img.shields.io/badge/License-GPLv3-blue.svg
+    ICS: No link found
+    MIT: https://img.shields.io/badge/License-MIT-yellow.svg
+
+    If there is no license, return an empty string
+  */
+}
 
 // TODO: Create a function that returns the license link
-// If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  /* License links
+    
+    Apache 2.0: https://opensource.org/licenses/Apache-2.0
+    GNU GPL 3.0: https://www.gnu.org/licenses/gpl-3.0
+    ICS: https://choosealicense.com/licenses/isc/
+    MIT: https://opensource.org/licenses/MIT
+
+    If there is no license, return an empty string
+  */
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
@@ -12,9 +30,55 @@ function renderLicenseSection(license) {}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
+  /*Put all the data together in the format is should be rendered in.
+  
+    Title
+    
+    [license badge]
+
+    Description
+
+    [description text]
+
+    Table of Contents (optional)
+
+      - Installation
+      - Usage
+      - License
+      - Contributing
+      - Tests
+      - Questions
+    
+    Installation
+
+    [installation text]
+
+    Usage
+
+    [usage text]
+
+    License
+
+    [license text with link]
+
+    Contributing
+
+    [contributing text]
+
+    Tests
+
+    [test text]
+
+    Questions
+
+    If you have any questions, contact the developer at:
+      -GitHub: [github name with link]
+      -Email: [email address]
+
+  */
   return `# ${data.title}
 
 `;
 }
 
-module.exports = generateMarkdown;
+module.exports = GenerateMarkdown;
